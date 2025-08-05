651a2e19de70b45dc7dfbb9e368cb92328bba9b9
zna15 <2232252995@qq.com>
2025-08-06 01:48:34 +0800

    修复部分配置的描述不生效的问题

--- src/main/java/org/uiop/easyplacefix/config/easyPlacefixConfig.java
+++ src/main/java/org/uiop/easyplacefix/config/easyPlacefixConfig.java
@@ -1,25 +1,30 @@
 package org.uiop.easyplacefix.config;
 
+import fi.dy.masa.litematica.util.BlockInfoListType;
 import fi.dy.masa.malilib.config.IConfigBase;
 import fi.dy.masa.malilib.config.options.ConfigBoolean;
 import fi.dy.masa.malilib.config.options.ConfigBooleanHotkeyed;
+import fi.dy.masa.malilib.config.options.ConfigOptionList;
+import fi.dy.masa.malilib.test.ConfigTestEnum;
 
 public final class easyPlacefixConfig {
     public static final ConfigBooleanHotkeyed LOOSEN_MODE =
-            new ConfigBooleanHotkeyed("loosenMode", false, "EasyPlaceFix.config.generic.comment.loosenMode");
+            new ConfigBooleanHotkeyed("loosenMode", false, "","EasyPlaceFix.config.generic.comment.loosenMode");
 
     public static final ConfigBooleanHotkeyed IGNORE_NBT =
-            new ConfigBooleanHotkeyed("nbtIgnore", false, "EasyPlaceFix.config.generic.comment.nbtIgnore");
+            new ConfigBooleanHotkeyed("nbtIgnore", false, "","EasyPlaceFix.config.generic.comment.nbtIgnore");
     public static final ConfigBooleanHotkeyed Allow_Interaction =
-            new ConfigBooleanHotkeyed("AllowInteraction", false, "EasyPlaceFix.config.generic.comment.AllowInteraction");
+            new ConfigBooleanHotkeyed("AllowInteraction", false, "","EasyPlaceFix.config.generic.comment.AllowInteraction");
     public static final ConfigBooleanHotkeyed OBSERVER_DETECT =
             new ConfigBooleanHotkeyed("observerDetect", false,"","EasyPlaceFix.config.generic.comment.observerDetect");
     public static final ConfigBooleanHotkeyed ENABLE_FIX =
             new ConfigBooleanHotkeyed("enableFix", false,"","EasyPlaceFix.config.generic.comment.enableFix");
-
+    public static final ConfigBooleanHotkeyed CLIENT_ROTATION_REVERT =
+            new ConfigBooleanHotkeyed("clientRotationRevert", false,"","EasyPlaceFix.config.generic.comment.clientRotationRevert","Rotation Revert","客户端朝向恢复");
 //    public static final ConfigBoolean Chain_Mode =
 //            new ConfigBoolean("ChainMode", true, "EasyPlaceFix.config.generic.comment.ChainMode");
-
+    public static final ConfigOptionList AAA =
+        new ConfigOptionList("1", BlockInfoListType.ALL);
 
     public static IConfigBase[] getExtraGenericConfigs() {
         return new IConfigBase[]{
@@ -28,6 +33,8 @@ public final class easyPlacefixConfig {
                 IGNORE_NBT,
                 Allow_Interaction,
                 OBSERVER_DETECT,
+                CLIENT_ROTATION_REVERT,
+                 AAA
         };
     }
 }
